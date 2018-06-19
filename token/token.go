540ec042adac496f2d0fffa9b674510609463444
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-06-20 00:54:53 +0900

    tokenを追加

---
+++ token/token.go
@@ -0,0 +1,33 @@
+///
+// File:  token.go
+// Author: ymiyamoto
+//
+// Created on Wed Jun 20 00:17:56 2018
+//
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	IDENT   = "IDENT"
+	INT     = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	SEMICOLON = ";"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	LIteral string
+}
