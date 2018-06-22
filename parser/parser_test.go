aee5fb1c48242208d04164c3c9f83ca10e1a03eb
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-06-23 07:00:04 +0900

    let-statementの変数部をparseする

---
+++ parser/parser_test.go
@@ -0,0 +1,72 @@
+///
+// File:  parser_test.go
+// Author: ymiyamoto
+//
+// Created on Sat Jun 23 06:22:03 2018
+//
+package parser
+
+import (
+	"testing"
+
+	"github.com/MiyamonY/monkey/ast"
+	"github.com/MiyamonY/monkey/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatal("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatment(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatment(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatemtn. got=%T", s)
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s' .got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral not '%s. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	return true
+}
