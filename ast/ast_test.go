f0fd47f506879d92b6b5725edefe79c1743a3c72
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-06-23 18:04:50 +0900

    ASTを文字列で出力できる

---
+++ ast/ast_test.go
@@ -0,0 +1,36 @@
+//
+//
+// File:  ast_test.go
+// Author: ymiyamoto
+//
+// Created on Sat Jun 23 16:56:11 2018
+//
+
+package ast
+
+import (
+	"testing"
+
+	"github.com/MiyamonY/monkey/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVal"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
